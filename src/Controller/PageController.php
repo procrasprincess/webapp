cc360a10619a132cbc7ffc99cfcf7e83738b5369
Andrew Lehman <aplehm@gmail.com>
2019-04-13 14:32:10 -0400

    Install annotations

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,9 +4,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+    /**
+     * @Route("/", name="app_homepage")
+     */
     public function homepage() {
         return $this->render('home.html.twig');
     }
