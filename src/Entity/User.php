c83c4e7b52b8b780555c1fd3d57abd5bae3f8445
ChexWarrior <aplehm@gmail.com>
2019-04-20 19:13:15 +0000

    Add user registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
